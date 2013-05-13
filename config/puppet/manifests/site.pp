6ff5eb5eda548c3d9c3228130decb9b88aadf226
petems <p.morsou@gmail.com>
2013-05-13 17:00:35 +0100

    Fix file

--- config/puppet/manifests/site.pp
+++ config/puppet/manifests/site.pp
@@ -54,7 +54,7 @@ require => Class["nginx"]
   file {
     '/etc/ssl/openssl.cnf' :
     ensure  => present,
-    source  => template('/capistrano-nginx-ssl/capistrano-nginx-ssl/current/config/puppet/templates/openssl.cnf.erb');
+    source  => template('/capistrano-nginx-ssl/capistrano-nginx-ssl/current/config/puppet/files/modules/openssl/openssl.cnf.erb');
     "/etc/ssl/CA/certs" :
     ensure  => directory,
     require => File['/etc/ssl/CA'];
